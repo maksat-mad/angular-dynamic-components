e461d0de1a745b88f0f7f2f4f03c69c25b42bc01
Maksat <maksat.madeniyetov@gmail.com>
2023-08-14 12:52:32 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AdService } from './services/ad.service';
+import { AdItem } from './utils/ad-item';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <div>
+      <app-ad-banner [ads]="ads"></app-ad-banner>
+    </div>
+  `
 })
-export class AppComponent {
-  title = 'angular-dynamic-components';
+export class AppComponent implements OnInit {
+  ads: AdItem[] = [];
+
+  constructor(private adService: AdService) {}
+
+  ngOnInit() {
+    this.ads = this.adService.getAds();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroJobAdComponent } from './components/hero-job-ad.component';
+import { HeroProfileComponent } from './components/hero-profile.component';
+import { AdBannerComponent } from './components/ad-banner.component';
+import { AdService } from './services/ad.service';
+import { AdDirective } from './directives/ad.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroJobAdComponent,
+    HeroProfileComponent,
+    AdBannerComponent,
+    AdDirective,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [AdService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
